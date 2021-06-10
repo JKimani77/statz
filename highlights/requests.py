06a6f20068eb77af35fdbdcf02f1fd2a9bfcdf99
JKimani77 <jkkimani77@gmail.com>
2021-06-10 17:15:21 +0300

    removed unused code in request.py

--- highlights/requests.py
+++ highlights/requests.py
@@ -21,17 +21,17 @@ def get_apistuff():
     '''
     function to get json response to request
     '''
-    url = "https://free-football-soccer-videos.p.rapidapi.com/?rapidapi-key=6c61f61a64mshb7f6dd3b49823b4p1ea082jsnbd55b4b7071b"
     
-    with urllib.request.urlopen(url)as url:
-        apidata = url.read()
-        apiresponse = json.loads(apidata)
+    
+    #with urllib.request.urlopen(url)as url:
+        #apidata = url.read()
+        #apiresponse = json.loads(apidata)
 
-        apiresults = []
+        #apiresults = []
 
-        if apiresponse['items']:
-            apilist = apiresponse['items']
-            apiresults = process_apiresults(apilist)
+        #if apiresponse['items']:
+            #apilist = apiresponse['items']
+            #apiresults = process_apiresults(apilist)
 
-    return apiresults
-
+    #return apiresults
+pass
